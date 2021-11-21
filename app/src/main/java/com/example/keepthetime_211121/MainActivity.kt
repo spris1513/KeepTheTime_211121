0a317f26062904961ea80fc7fedd75a9906ef2e7
spris1513 <89328944+spris1513@users.noreply.github.com>
2021-11-21 11:25:50 +0900

    회원가입 화면 UI + 이동하기

--- app/src/main/java/com/example/keepthetime_211121/MainActivity.kt
+++ app/src/main/java/com/example/keepthetime_211121/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.keepthetime_211121
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -24,6 +25,11 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.btnSignUp.setOnClickListener {
+            val myIntent = Intent(mContext, SignUpActivity::class.java)
+            startActivity(myIntent)
+        }
 
         binding.btnLogin.setOnClickListener {
 
