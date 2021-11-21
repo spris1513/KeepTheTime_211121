dee729accb880d9180ba2cbeab3eb7bcfc097225
spris1513 <89328944+spris1513@users.noreply.github.com>
2021-11-21 15:44:48 +0900

    프래그먼트 데이터 바인딩 체험

--- app/src/main/java/com/example/keepthetime_211121/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/example/keepthetime_211121/fragments/MyProfileFragment.kt
@@ -1,13 +1,23 @@
 package com.example.keepthetime_211121.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.keepthetime_211121.R
+import com.example.keepthetime_211121.databinding.FragmentMyProfileBinding
+import com.example.keepthetime_211121.datas.BasicResponse
+import com.example.keepthetime_211121.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyProfileFragment : BaseFragment() {
+
+    lateinit var binding : FragmentMyProfileBinding
 
 
     override fun onCreateView(
@@ -15,7 +25,8 @@ class MyProfileFragment : BaseFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_profile,container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_profile,container,false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -29,10 +40,30 @@ class MyProfileFragment : BaseFragment() {
 
     override fun setValues() {
 //        내 정보를 서버에서 받아오자 > 이미지 반영 / 닉네임 반영
+        getMyInfoFromServer()
 
 //        1. 프래그먼트에서 retrofit 어떻게 활용?
 
 //        2. dataBinding > 프래그먼트에서는 어떻게 데이터 바인딩?
 
     }
+
+    fun getMyInfoFromServer(){
+        apiService.getRequestMyInfo(ContextUtil.getToken(mContext)).enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful){
+                    val br = response.body()!!
+
+//                    닉네임 / 프로필 이미지 주소 추출 > UI 반영??
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+        })
+    }
+
 }
