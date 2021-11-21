a15d0a0ea373a3afb21b1d5060bb0b69396ab846
spris1513 <89328944+spris1513@users.noreply.github.com>
2021-11-21 11:21:40 +0900

    회원가입 화면추가

---
+++ app/src/main/java/com/example/keepthetime_211121/SignUpActivity.kt
@@ -0,0 +1,26 @@
+package com.example.keepthetime_211121
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.keepthetime_211121.databinding.ActivitySignUpBinding
+
+class SignUpActivity : BaseActivity() {
+
+    lateinit var binding : ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
