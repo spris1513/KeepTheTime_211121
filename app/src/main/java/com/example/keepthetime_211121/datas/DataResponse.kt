efd647eaad25abfde017927724724b758c56714e
spris1513 <89328944+spris1513@users.noreply.github.com>
2021-11-21 11:46:01 +0900

    사용자 데이터 파싱 클래스 추가  - 로그인 API의 응답이 > 사용자까지 모두 파싱해줬다.

--- app/src/main/java/com/example/keepthetime_211121/datas/DataResponse.kt
+++ app/src/main/java/com/example/keepthetime_211121/datas/DataResponse.kt
@@ -1,6 +1,7 @@
 package com.example.keepthetime_211121.datas
 
 class DataResponse(
+    var user : UserData,
     var token : String
 ) {
 }
---
+++ app/src/main/java/com/example/keepthetime_211121/datas/UserData.kt
@@ -0,0 +1,19 @@
+package com.example.keepthetime_211121.datas
+
+import com.google.gson.annotations.SerializedName
+
+class UserData(
+
+    var id:Int,
+    var provider:String,
+    var uid: String?,
+    var email : String,
+    @SerializedName("ready_minute")
+    var readyMinute:Int,
+    @SerializedName("nick_name")
+    var nickname : String,
+
+
+
+) {
+}
