c314c8da001f203528ab5cc7ffa707dc9845826d
spris1513 <89328944+spris1513@users.noreply.github.com>
2021-11-21 15:20:52 +0900

    BaseFragment : 레트로핏 관련 변수 상속처리

--- app/src/main/java/com/example/keepthetime_211121/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/keepthetime_211121/fragments/BaseFragment.kt
@@ -3,15 +3,27 @@ package com.example.keepthetime_211121.fragments
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.example.keepthetime_211121.api.ServerAPI
+import com.example.keepthetime_211121.api.ServerAPIService
 import com.example.keepthetime_211121.utils.ContextUtil
+import retrofit2.Retrofit
 
 abstract class BaseFragment : Fragment() {
 
     lateinit var mContext : Context
 
+//    모든 프래그먼트에서도 서버 통신이 필요.
+
+    private lateinit var retrofit : Retrofit
+    lateinit var apiService : ServerAPIService
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         mContext= requireContext()
+
+        retrofit= ServerAPI.getRetrofit()
+        apiService = retrofit.create(ServerAPIService::class.java)
+
     }
 
     abstract fun setupEvents()
