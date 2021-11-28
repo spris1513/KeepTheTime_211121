f23f7bc877377fcbbadb55f8b08ef7bac2a57d1f
spris1513 <89328944+spris1513@users.noreply.github.com>
2021-11-28 14:14:28 +0900

    친구 찾기 /  추가화면 UI

--- app/src/main/java/com/example/keepthetime_211121/ViewFriendListActivity.kt
+++ app/src/main/java/com/example/keepthetime_211121/ViewFriendListActivity.kt
@@ -1,5 +1,6 @@
 package com.example.keepthetime_211121
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -29,6 +30,15 @@ class ViewFriendListActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        binding.btnAddFriend.setOnClickListener {
+
+            //        친구 추가 화면으로 이동
+
+            val myIntent = Intent(mContext,AddFriendActivity::class.java)
+            startActivity(myIntent)
+        }
+
+
 
     }
 
