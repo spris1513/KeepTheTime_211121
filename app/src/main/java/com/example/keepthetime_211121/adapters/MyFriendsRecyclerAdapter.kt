0f9bffc7aa657e95f9292d55f41c0b19c56311ef
spris1513 <89328944+spris1513@users.noreply.github.com>
2021-11-28 12:36:37 +0900

    리싸이클러 어댑터 기초작업

---
+++ app/src/main/java/com/example/keepthetime_211121/adapters/MyFriendsRecyclerAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.keepthetime_211121.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.keepthetime_211121.R
+import com.example.keepthetime_211121.datas.UserData
+
+class MyFriendsRecyclerAdapter(val mContext : Context , val mList : ArrayList<UserData>) : RecyclerView.Adapter<MyFriendsRecyclerAdapter.MyFriendViewHolder>() {
+
+    inner class MyFriendViewHolder(row : View) : RecyclerView.ViewHolder(row) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyFriendViewHolder {
+
+        val row = LayoutInflater.from(mContext).inflate(R.layout.my_friend_list_item, parent, false)
+        return MyFriendViewHolder(row)
+
+    }
+
+    override fun onBindViewHolder(holder: MyFriendViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = mList.size
+
+    }
+
+}
