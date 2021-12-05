7ece6cf0d48633cc5b0f1a2d037989daf3a18cc5
spris1513 <89328944+spris1513@users.noreply.github.com>
2021-12-05 10:45:35 +0900

    Calendar 클래스 활용(2)  - 연/월/일을 한번에 저장하는 set기능 활용

--- app/src/main/java/com/example/keepthetime_211121/EdtAppointmentActivity.kt
+++ app/src/main/java/com/example/keepthetime_211121/EdtAppointmentActivity.kt
@@ -41,7 +41,16 @@ class EdtAppointmentActivity : BaseActivity() {
 
 //                    실제로 날짜가 선택되면 할일 적는 공간
 
-                    Log.d("선택한년월일","${year}년 ${month}월 ${dayOfMonth}일 선택됨")
+//                    Log.d("선택한년월일","${year}년 ${month}월 ${dayOfMonth}일 선택됨")
+
+//                    선택된 일시를 저장할 변수에, 연/월/일을 저장
+
+//                    mSelectedDateTime.set(Calendar.YEAR,year)
+//                    mSelectedDateTime.set(Calendar.MONTH,month)
+//                    mSelectedDateTime.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+
+//                    년/월/일을 한번에 저장하는 set 함수 활용
+                    mSelectedDateTime.set(year,month,dayOfMonth)
 
                 }
 
@@ -52,6 +61,7 @@ class EdtAppointmentActivity : BaseActivity() {
 //            Kotlin : JAVA 기반 언어 > 월: 0 ~ 11 로 만들어져있음.
 
 //            오늘 날짜를 기본으로 띄우도록 > mSelectedDateTime에 저장된 값을 활용
+//            선택한 이후로는 > 이전에 선택한 날짜가
 
             val datePickerDialog = DatePickerDialog(
                 mContext,
