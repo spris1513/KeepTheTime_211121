7bdb215b00a7458f2825195f6b9da3f193ac5db7
spris1513 <89328944+spris1513@users.noreply.github.com>
2021-12-05 15:50:20 +0900

    네이버 맵 객체 추출  - 스케쥴데이터 받아서 활용해야함

--- app/src/main/java/com/example/keepthetime_211121/ViewPlaceMapActivity.kt
+++ app/src/main/java/com/example/keepthetime_211121/ViewPlaceMapActivity.kt
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_211121.databinding.ActivityViewPlaceMapBinding
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.overlay.Marker
 
 class ViewPlaceMapActivity : BaseActivity() {
 
     lateinit var binding : ActivityViewPlaceMapBinding
 
+    var mSelectedLatLng : LatLng? = null
+    var mSelectedMarker : Marker? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=DataBindingUtil.setContentView(this,R.layout.activity_view_place_map)
+        binding.naverMapView.onCreate(savedInstanceState)
         setupEvents()
         setValues()
     }
@@ -24,11 +31,52 @@ class ViewPlaceMapActivity : BaseActivity() {
 
 //        0. 프로젝트에 네이버지도 설치(완료)
 
-//        1. 화면(xml)에 네이버 맵 띄워주기
+//        1. 화면(xml)에 네이버 맵 띄워주기(완료)
 
 //        2. 네이버맵 객체를 실제로 얻어내기 > getMapAsync
 
-//        3. 카메라 이동 / 마커 추가
+        binding.naverMapView.getMapAsync {
 
+            val naverMap = it
+
+
+
+        }
+
+//        3. 카메라 이동 / 마커 추가
+    }
+    override fun onStart() {
+        super.onStart()
+        binding.naverMapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.naverMapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.naverMapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.naverMapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.naverMapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.naverMapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.naverMapView.onLowMemory()
     }
 }
