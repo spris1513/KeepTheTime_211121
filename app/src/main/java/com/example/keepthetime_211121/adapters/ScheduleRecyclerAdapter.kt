ebd00fd4d5de897218ad66f8699c944d8e26e390
spris1513 <89328944+spris1513@users.noreply.github.com>
2021-12-05 14:34:46 +0900

    스케쥴 목록 어댑터 기초작업

---
+++ app/src/main/java/com/example/keepthetime_211121/adapters/ScheduleRecyclerAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.keepthetime_211121.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.keepthetime_211121.R
+import com.example.keepthetime_211121.datas.ScheduleData
+
+class ScheduleRecyclerAdapter(val mContext : Context,val mList : List<ScheduleData>) : RecyclerView.Adapter<ScheduleRecyclerAdapter.ScheduleViewHolder>() {
+
+    inner class ScheduleViewHolder(row:View) :RecyclerView.ViewHolder(row){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScheduleViewHolder {
+
+        val row = LayoutInflater.from(mContext).inflate(R.layout.schedule_list_item,parent,false)
+        return ScheduleViewHolder(row)
+
+    }
+
+    override fun onBindViewHolder(holder: ScheduleViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = mList.size
+
+    }
+
+}
