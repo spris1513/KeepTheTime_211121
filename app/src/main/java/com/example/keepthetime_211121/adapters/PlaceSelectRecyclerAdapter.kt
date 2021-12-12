86a1661192a0cebfd07bd0e57b2e0cd430339981
spris1513 <89328944+spris1513@users.noreply.github.com>
2021-12-12 15:36:25 +0900

    리싸이클러뷰 한칸(ViewHolder - row)이 눌리면 할일 지정  - onItemClickListener에 할 일이 적혀있는지? 적혀있다면 그 내용대로 실행

--- app/src/main/java/com/example/keepthetime_211121/adapters/PlaceSelectRecyclerAdapter.kt
+++ app/src/main/java/com/example/keepthetime_211121/adapters/PlaceSelectRecyclerAdapter.kt
@@ -9,37 +9,53 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.keepthetime_211121.R
 import com.example.keepthetime_211121.datas.PlaceData
 
-class PlaceSelectRecyclerAdapter(val mContext : Context,val mList : ArrayList<PlaceData>) : RecyclerView.Adapter<PlaceSelectRecyclerAdapter.PlaceViewHolder>() {
+class PlaceSelectRecyclerAdapter(val mContext: Context, val mList: ArrayList<PlaceData>) :
+    RecyclerView.Adapter<PlaceSelectRecyclerAdapter.PlaceViewHolder>() {
 
 //    장소 선택 리싸이클러뷰의 클릭 이벤트 발생 > 액티비티 or 프래그먼트 등으로 할 일을 넘겨주자 ( interface 활용)
 
-    interface OnItemClickListener{
-        fun onItemClick(position: Int)
+    interface OnItemClickListener {
+        fun onItemClick(data: PlaceData)
     }
 
-    inner class PlaceViewHolder(row: View) : RecyclerView.ViewHolder(row){
+//    인터페이스를 변수로 설정
 
-//        멤버변수 - list_item 내부의 UI
+    //    null : 아이템 클릭시 할 일이 없다.
+//    null 아니다 : 할일이 지정된 상태가 아니다.
+    var onItemClickListener: OnItemClickListener? = null
+
+
+    inner class PlaceViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
+
+        //        멤버변수 - list_item 내부의 UI
         val txtPlaceName = row.findViewById<TextView>(R.id.txtPlaceName)
 
-//        함수 - 실제 데이터 연동 bind
-        fun bind(data : PlaceData){
+        //        함수 - 실제 데이터 연동 bind
+        fun bind(data: PlaceData) {
 
             txtPlaceName.text = data.placeName
+
+//              한 줄이 눌리면 할일
+            row.setOnClickListener {
+
+//                아이템클릭리스너에 적혀있는 코드 실행
+                onItemClickListener?.onItemClick(data)
+            }
+
 
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceViewHolder {
-        val row = LayoutInflater.from(mContext).inflate(R.layout.place_list_item,parent,false)
+        val row = LayoutInflater.from(mContext).inflate(R.layout.place_list_item, parent, false)
         return PlaceViewHolder(row)
 
     }
 
     override fun onBindViewHolder(holder: PlaceViewHolder, position: Int) {
 
-        holder.bind( mList[position] )
+        holder.bind(mList[position])
 
     }
 
