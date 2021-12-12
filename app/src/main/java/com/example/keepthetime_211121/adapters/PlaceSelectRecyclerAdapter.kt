c4b6a3cee5f87374915116ed5b7d9f3db1602d02
spris1513 <89328944+spris1513@users.noreply.github.com>
2021-12-12 14:58:42 +0900

    얼럿 내부에서 사용할 리싸이클러어댑터 생성중

---
+++ app/src/main/java/com/example/keepthetime_211121/adapters/PlaceSelectRecyclerAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.keepthetime_211121.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PlaceSelectRecyclerAdapter : RecyclerView.Adapter<PlaceSelectRecyclerAdapter.PlaceViewHolder>() {
+
+    inner class PlaceViewHolder(row: View) : RecyclerView.ViewHolder(row){
+
+    }
+
+}
