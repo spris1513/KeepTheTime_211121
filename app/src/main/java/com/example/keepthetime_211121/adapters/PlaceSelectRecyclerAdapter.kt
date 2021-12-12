ac8efbfab7c923f30bfa9636633832cb1cc648f7
spris1513 <89328944+spris1513@users.noreply.github.com>
2021-12-12 15:01:54 +0900

    리싸이클러 어댑터 뼈대작업 마무리

--- app/src/main/java/com/example/keepthetime_211121/adapters/PlaceSelectRecyclerAdapter.kt
+++ app/src/main/java/com/example/keepthetime_211121/adapters/PlaceSelectRecyclerAdapter.kt
@@ -1,12 +1,29 @@
 package com.example.keepthetime_211121.adapters
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.keepthetime_211121.R
+import com.example.keepthetime_211121.datas.PlaceData
 
-class PlaceSelectRecyclerAdapter : RecyclerView.Adapter<PlaceSelectRecyclerAdapter.PlaceViewHolder>() {
+class PlaceSelectRecyclerAdapter(val mContext : Context,val mList : ArrayList<PlaceData>) : RecyclerView.Adapter<PlaceSelectRecyclerAdapter.PlaceViewHolder>() {
 
     inner class PlaceViewHolder(row: View) : RecyclerView.ViewHolder(row){
 
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceViewHolder {
+        val row = LayoutInflater.from(mContext).inflate(R.layout.place_list_item,parent,false)
+        return PlaceViewHolder(row)
+
+    }
+
+    override fun onBindViewHolder(holder: PlaceViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = mList.size
 
 }
